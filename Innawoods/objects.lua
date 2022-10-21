e371b302dd82d83105768e8f6991815556779121
SageAzakaela <sageazakaela@gmail.com>
2022-10-21 15:58:53 -0600

    Initial Commit
    Added all the stuff we'll need and the framework for moving forward

---
+++ Innawoods/objects.lua
@@ -0,0 +1,5 @@
+objects = {
+  { name = "", type = "DeepForest", x = 300, y = 0, z = 0, width = 300, height = 300 },
+  { name = "", type = "SpawnPoint", x = 1611, y = 1756, z = 0, width = 1, height = 1, properties = { Professions = "all" } },
+  { name = "", type = "SpawnPoint", x = 1602, y = 1749, z = 0, width = 1, height = 1, properties = { Professions = "all" } }
+}
