9177485b81037c1f6928b900a52d99964ece39b5
QuietLerlyn <thevaleragame5004@gmail.com>
2022-10-26 17:47:25 +0300

    Small Update from Lerlyn
    26.10.2022. Finished village (maybe i will remade it in the future), made Powerlines Added new tiles - bird nests with some overlays (tile properties will be made later.)

--- Innawoods/tmxworld/spawnpoints.lua
+++ Innawoods/tmxworld/spawnpoints.lua
@@ -1,3 +1,9 @@
 function SpawnPoints()
-return {}
+return {
+  profession = {
+    { worldX = 5, worldY = 5, posX = 111, posY = 256, posZ = 0 },
+    { worldX = 5, worldY = 5, posX = 102, posY = 249, posZ = 0 },
+    { worldX = 5, worldY = 6, posX = 150, posY = 105, posZ = 0 }
+  }
+}
 end
