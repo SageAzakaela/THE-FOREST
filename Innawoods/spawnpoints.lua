d221bf9fef07b01e7c23391001562cc3bc34a04f
SageAzakaela <sageazakaela@gmail.com>
2022-10-27 20:38:06 -0600

    Finished Zoning my Area
    Added a huge maze as well, updated the pack file and yeah... pretty nifty gonna test this out shortly

--- Innawoods/spawnpoints.lua
+++ Innawoods/spawnpoints.lua
@@ -1,3 +1,8 @@
 function SpawnPoints()
-return {}
+return {
+  profession = {
+    { worldX = 5, worldY = 5, posX = 111, posY = 256, posZ = 0 },
+    { worldX = 5, worldY = 6, posX = 150, posY = 105, posZ = 0 }
+  }
+}
 end
